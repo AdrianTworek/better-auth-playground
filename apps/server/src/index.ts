20c64e3d151c072e2e4744dfd078f1453e5caebe
Adrian Tworek <adrian.tworek.it@gmail.com>
2025-02-04 12:11:21 +0100

    feat: setup server auth

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import { toNodeHandler } from 'better-auth/node';
+import { auth } from './lib/auth';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+app.all('/api/auth/**', toNodeHandler(auth));
 
 app.use(express.json());
 
