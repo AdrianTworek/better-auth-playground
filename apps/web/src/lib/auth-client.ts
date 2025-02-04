173eda68550db16d7ae8fb9dc0d7d07b2cda83b1
Adrian Tworek <adrian.tworek.it@gmail.com>
2025-02-04 16:00:35 +0100

    chore: env examples

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from 'better-auth/react';
 
 export const { useSession, signUp, signIn, signOut } = createAuthClient({
-  baseURL: 'http://localhost:4000',
+  baseURL: import.meta.env.VITE_AUTH_BASE_URL || 'http://localhost:4000',
 });
