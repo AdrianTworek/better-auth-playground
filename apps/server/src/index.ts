80dd6dfc49b8ea6b0acdaf1ed753df1c324ff7e2
Adrian Tworek <adrian.tworek.it@gmail.com>
2025-02-04 13:09:14 +0100

    chore: remove next.js

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,9 +1,17 @@
 import express from 'express';
 import { toNodeHandler } from 'better-auth/node';
 import { auth } from './lib/auth';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }),
+);
 
 app.all('/api/auth/**', toNodeHandler(auth));
 
--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -1,12 +1,15 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from '../db';
+import * as schema from '../db/schema';
 
 const auth = betterAuth({
   secret: process.env.BETTER_AUTH_SECRET,
   url: process.env.BETTER_AUTH_URL,
+  trustedOrigins: ['http://localhost:3000'],
   database: drizzleAdapter(db, {
     provider: 'pg',
+    schema,
   }),
   emailAndPassword: {
     enabled: true,
